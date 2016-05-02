e6a27b222d47209b3e8f9bbc3637c21dc4ba002f
Kociamber <koci@o2.pl>
2016-05-02 17:08:31 +0200

    add polish locale and make it default

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,6 +1,7 @@
 module MembersHelper
   def sort_link(column, title = nil)
-    title ||= column.titleize
+    #title ||= column.titleize
+    title ||= Member.human_attribute_name(column).titleize
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     icon = sort_direction == "asc" ? "glyphicon glyphicon-chevron-up" : "glyphicon glyphicon-chevron-down"
     icon = column == sort_column ? icon : ""
