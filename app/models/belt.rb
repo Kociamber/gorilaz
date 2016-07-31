a33ab7b2e3b71bd0090594c9ec03bd2235b1af1f
Kociamber <koci@o2.pl>
2016-07-31 17:02:39 +0200

    add belts table migration, set up basic validations and relations

---
+++ app/models/belt.rb
@@ -0,0 +1,9 @@
+class Belt < ActiveRecord::Base
+
+  belongs_to :member
+
+  validates :belt, inclusion: { in: 1..5, message: "must be selected" }
+  validates :stripes, inclusion: { in: 0..4, message: "number must be selected" }
+  validates :promotion_date, presence: true
+
+end
